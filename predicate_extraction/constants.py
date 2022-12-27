8d9915b1b32696c6ec30abeba8735475d1d6d67f
Pedro Ligabue <pedroligabue@gmail.com>
2022-12-27 02:13:53 -0300

    Add BIO tags as an Enum

--- predicate_extraction/constants.py
+++ predicate_extraction/constants.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+from enum import Enum
 
 MAX_SENTENCE_SIZE = 50
 PREDICATE_PREDICTION_DIR = Path(__file__).parent
 PREDICATE_PATTERN = r"\[(.*)\]"
+BIO = Enum("BIO", ["B", "I", "O"])
