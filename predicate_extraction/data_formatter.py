f18b78f791c0fe059c6278e8cfa9eb278789914f
Pedro Ligabue <pedroligabue@gmail.com>
2022-12-27 02:14:53 -0300

    Add BIO tags to data formatter

--- predicate_extraction/data_formatter.py
+++ predicate_extraction/data_formatter.py
@@ -1,9 +1,10 @@
 import yaml
 from yaml import CLoader as Loader, CDumper as Dumper
 import re
+import tensorflow as tf
 from transformers import AutoTokenizer
 
-from .constants import MAX_SENTENCE_SIZE, PREDICATE_PREDICTION_DIR, PREDICATE_PATTERN
+from .constants import BIO, PREDICATE_PREDICTION_DIR, PREDICATE_PATTERN, MAX_SENTENCE_SIZE
 
 tokenizer = AutoTokenizer.from_pretrained("neuralmind/bert-base-portuguese-cased")
 
@@ -14,17 +15,23 @@ def split_on_predicate(sentence):
 
 def format_sentence(sentence):
     split_sentence = split_on_predicate(sentence)
+    original_sentence = " ".join(split_sentence)
+
     token_sets = [tokenizer.encode(chunk, add_special_tokens=False) for chunk in split_sentence]
     predicate_start = len(token_sets[0]) + 1    # 1 is added to account for the [CLS] token that is added later
     predicate_end = predicate_start + len(token_sets[1])
-    input_tokens = [101] + [token for token_set in token_sets for token in token_set] + [102]
-    output_tags = [("P-B" if i == predicate_start else "P-I" if predicate_start < i < predicate_end else "P-O") for i, token in enumerate(input_tokens)]
+
+    input_tokens = tokenizer.encode(original_sentence, padding="max_length", max_length=MAX_SENTENCE_SIZE)
+    output_tags = [(BIO.B if i == predicate_start else BIO.I if predicate_start < i < predicate_end else BIO.O) for i, token in enumerate(input_tokens)]
+    encoded_output_tags = tf.one_hot([tag.value - 1 for tag in output_tags], len(BIO))
     
     return {
+        "sentence": original_sentence,
         "token_ids": input_tokens,
         "tokens": tokenizer.convert_ids_to_tokens(input_tokens),
         "predicate_span": (predicate_start, predicate_end),
-        "output_tags": output_tags
+        "output_tags": output_tags,
+        "encoded_output_tags": encoded_output_tags
     }
 
 def format_data(sentences):
