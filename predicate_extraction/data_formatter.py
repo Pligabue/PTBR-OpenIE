6eb451bef48c0ff183873996087af6c002b737da
Pedro Ligabue <pedroligabue@gmail.com>
2022-12-31 02:23:04 -0300

    Add methods to format inputs and outputs

--- predicate_extraction/data_formatter.py
+++ predicate_extraction/data_formatter.py
@@ -13,6 +13,9 @@ def split_on_predicate(sentence):
     trimmed_split = [chunk.strip() for chunk in split_sentence]
     return trimmed_split
 
+def format_input(sentence):
+    return tokenizer.encode(sentence, padding="max_length", max_length=MAX_SENTENCE_SIZE)
+
 def format_sentence(sentence):
     split_sentence = split_on_predicate(sentence)
     original_sentence = " ".join(split_sentence)
@@ -21,7 +24,7 @@ def format_sentence(sentence):
     predicate_start = len(token_sets[0]) + 1    # 1 is added to account for the [CLS] token that is added later
     predicate_end = predicate_start + len(token_sets[1])
 
-    input_tokens = tokenizer.encode(original_sentence, padding="max_length", max_length=MAX_SENTENCE_SIZE)
+    input_tokens = format_input(original_sentence)
     output_tags = [(BIO.B if i == predicate_start else BIO.I if predicate_start < i < predicate_end else BIO.O) for i, token in enumerate(input_tokens)]
     encoded_output_tags = tf.one_hot([tag.value - 1 for tag in output_tags], len(BIO))
     
@@ -34,10 +37,30 @@ def format_sentence(sentence):
         "encoded_output_tags": encoded_output_tags
     }
 
-def format_data(sentences):
+def format_sentences(sentences):
     return [format_sentence(sentence) for sentence in sentences]
+
+def format_output(output):
+    predicted_tag_values = tf.math.argmax(output, 1) + 1
+    predicted_tags = [BIO(prediction).name for prediction in predicted_tag_values]
+    return predicted_tags
+
+def print_annotated_sentence(sentence, tags):
+    token_ids = tokenizer.encode(sentence)
+    tokens = tokenizer.convert_ids_to_tokens(token_ids)
+    
+    sentence_chunks = []
+    annotation_chunks = []
+    for i, token in enumerate(tokens):
+        sentence_chunks.append(token)
+        annotation_chunks.append(f"{tags[i]:<{len(token)}}")
+
+    token_sentence = " ".join(sentence_chunks)
+    annotation = " ".join(annotation_chunks)
+    
+    return token_sentence + "\n" + annotation
 
 if __name__ == "__main__":
     with (PREDICATE_PREDICTION_DIR / "training_data.yml").open(encoding="utf-8") as f:
         sentences = yaml.load(f, Loader)
-    format_data(sentences)
+    format_sentences(sentences)
