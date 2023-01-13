d72f594ef9f4d16151296dfc8e2708d646de591d
Pedro Ligabue <pedroligabue@gmail.com>
2023-01-13 16:33:07 -0300

    Update requirements.txt
    Mainly downgraded TensorFlow to v2.10, which is the last one with native
    GPU support for Windows.
    
    Related: https://blog.tensorflow.org/2022/09/announcing-tensorflow-official-build-collaborators.html?_gl=1*d0tjpq*_ga*MjA3NDc5OTc0LjE2MzAxMDE3NDg.*_ga_W0YLR4190T*MTY3MzYzNTg2OC4xMy4xLjE2NzM2MzgyNTcuMC4wLjA.

--- predicate_extraction/model.py
+++ predicate_extraction/model.py
@@ -40,9 +40,7 @@ class PredicateExtractor(DataFormatter):
         early_stopping_callback = tf.keras.callbacks.EarlyStopping(
             monitor='loss',
             patience=math.ceil(epochs/20 if epochs > 60 else 3),
-            start_from_epoch=math.ceil(epochs/10),
-            min_delta=0.0005
-        )
+            min_delta=0.0005)
         callbacks = callbacks or [early_stopping_callback] if early_stopping else []
 
         return self.model.fit(training_x, training_y, *args, epochs=epochs, callbacks=callbacks, **kwargs)
