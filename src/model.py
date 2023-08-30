634275193bf9f72c1b2f1ea60176a1a8300c51a4
Pedro Ligabue <pedroligabue@gmail.com>
2023-08-30 19:11:21 -0300

    Add methods to save/load the triple extract. model
    Similar to the corresponding methods in the pred. extract. and arg.
    pred. models.

--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 
 from typing import Union
 
+from .constants import DEFAULT_MODEL_NAME
 from .argument_prediction import ArgumentPredictor
 from .predicate_extraction import PredicateExtractor
 from .data_formatter import DataFormatter
@@ -20,3 +21,11 @@ class TripleExtractor(DataFormatter):
             self.argument_predictor = ArgumentPredictor(*ap_layers_or_name)
 
         super().__init__(self.predicate_extractor.tokenizer)
+
+    @classmethod
+    def load(cls, name: str = DEFAULT_MODEL_NAME):
+        return cls(name, name)
+
+    def save(self, name: str = DEFAULT_MODEL_NAME):
+        self.predicate_extractor.save(name)
+        self.argument_predictor.save(name)
